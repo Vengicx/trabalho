649498e79ee18895b50bb4f570a37c86256a6e9a
Giovanni Vengi7 <kitocx@gmail.com>
2018-05-31 00:29:42 -0300

    verificação de Login
    Está quase terminada, amanhã irei à igreja ajudar meus amigos na missa de Corpus Christi, depois vou continuar o site!

--- admin/app/verifyLogin.php
+++ admin/app/verifyLogin.php
@@ -4,7 +4,7 @@
 	//verificar se o usuário esta logado
 	if (!isset($_SESSION["sistema"]["id"])) {
 
-		header("Location: index.php");
+		header("Location: ../index.php");
 		
 	}*/
 
@@ -17,18 +17,28 @@
 		
 		if (isset ($_POST["senha"])){
 			$senha = trim ($_POST["senha"]);
+			// $senha = password_hash ($senha, PASSWORD_DEFAULT);
 		}
 		/* fim da area do index.php */
+		/* area de validacao no banco de dados */
 
+		if(empty($_POST["senha"])){
+			echo "<script>alert('Digite sua senha');history.back;</script>";
+			exit;
+		}elseif(empty($_POST["login"])){
+			echo "<script>alert('Digite sua senha');history.back;</script>";
+			exit;
+		}else{
+			include "conecta.php";
+			$sql = "select * from usuario";
+			$consulta->prepare($sql);
 
-		
-
-
-
-
-
-
-
+			if($consulta->execute()){
+				$_SESSION["sistema"]["id"];
+			}else{
+				
+			}
+		}
 
 
 	}else{		//fim do if($_POST)
